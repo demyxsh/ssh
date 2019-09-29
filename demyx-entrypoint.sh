4d66da992040361f3cd020637df75417ccf41c7c
Demyx <info@demyx.sh>
2019-09-29 03:08:40 -0700

    Add comments and minor updates

--- demyx-entrypoint.sh
+++ demyx-entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-fi
-
+# Prevents ssh errors from local machine
 if [ -f /home/www-data/.ssh/ssh_host_rsa_key ]; then
 	cp /home/www-data/.ssh/ssh_host_rsa_key /etc/ssh
 	cp /home/www-data/.ssh/ssh_host_rsa_key.pub /etc/ssh
 else
+	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 	cp /etc/ssh/ssh_host_rsa_key /home/www-data/.ssh
 	cp /etc/ssh/ssh_host_rsa_key.pub /home/www-data/.ssh
 fi
 
-chown -R www-data:www-data /home/www-data
+# Set proper ssh permissions
+if [[ -f /home/www-data/.ssh/authorized_keys ]]; then
+	chmod 644 /home/www-data/.ssh/authorized_keys
+fi
 chmod 700 /home/www-data/.ssh
-chmod 644 /home/www-data/.ssh/authorized_keys
 chmod 600 /etc/ssh/ssh_host_rsa_key
+chown -R www-data:www-data /home/www-data
 
 /usr/sbin/sshd -D
